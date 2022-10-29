5176998b22df7b28d0e3d4cd175afa00e5bf6816
davidcnitu <dcn.programming@gmail.com>
2022-10-29 20:53:57 +0100

    style: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from "../images/react-logo.png";
 
 export default function () {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img alt="logo" src={logo} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     );
 }
